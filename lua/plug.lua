8bac75381ad44d640f13a0e154136edf1575195b
QiyJarw <mitsurun0515@yahoo.co.jp>
2022-01-18 00:31:48 +0900

    add: added plugins;bufferline.nvim,nvim-tree.lua

--- lua/plug.lua
+++ lua/plug.lua
@@ -2,12 +2,12 @@
 vim.cmd([[
 call plug#begin('C:/Users/mitsu/AppData/Local/nvim/plugs/')
 Plug 'nvim-lua/plenary.nvim' "deps on telescope
-Plug 'lambdalisue/fern.vim'
-Plug 'lambdalisue/fern-renderer-nerdfont.vim'
-Plug 'lambdalisue/nerdfont.vim'
-Plug 'yuki-yano/fern-preview.vim'
-Plug 'lambdalisue/fern-git-status.vim'
-Plug 'lambdalisue/glyph-palette.vim'
+" Plug 'lambdalisue/fern.vim', 
+" Plug 'lambdalisue/fern-renderer-nerdfont.vim',
+" Plug 'lambdalisue/nerdfont.vim',
+" Plug 'lambdalisue/fern-git-status.vim',
+" Plug 'lambdalisue/glyph-palette.vim',
+" Plug 'yuki-yano/fern-preview.vim',
 Plug 'neovim/nvim-lspconfig'
 Plug 'hrsh7th/vim-vsnip'
 Plug 'hrsh7th/nvim-cmp'
@@ -16,10 +16,13 @@ Plug 'hrsh7th/cmp-vsnip'
 Plug 'hrsh7th/cmp-buffer'
 Plug 'onsails/lspkind-nvim'
 Plug 'williamboman/nvim-lsp-installer'
+Plug 'kyazdani42/nvim-web-devicons'
+Plug 'kyazdani42/nvim-tree.lua'
+Plug 'akinsho/bufferline.nvim'
 Plug 'nvim-lualine/lualine.nvim'
 Plug 'nvim-lua/lsp-status.nvim'
 Plug 'folke/trouble.nvim'
-Plug 'kdheepak/tabline.nvim'
+" Plug 'kdheepak/tabline.nvim'
 Plug 'terryma/vim-multiple-cursors'
 Plug 'tpope/vim-surround'
 Plug 'alvan/vim-closetag'
@@ -34,7 +37,6 @@ Plug 'nvim-telescope/telescope.nvim'
 Plug 'tom-anders/telescope-vim-bookmarks.nvim'
 Plug 'akinsho/toggleterm.nvim'
 Plug 'folke/todo-comments.nvim'
-Plug 'kyazdani42/nvim-web-devicons'
 Plug 'gko/vim-coloresque'
 Plug 'iamcco/markdown-preview.nvim', {'do': 'cd app && yarn install'}
 "themes
