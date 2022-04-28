aaba6bafe445fe4a0c72549021aa124e16832be2
QiyJarw <mitsurun0515@yahoo.co.jp>
2022-04-28 21:42:25 +0900

    update: 主な言語のLSに限定

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,8 +1,8 @@
 --nvim-treesitter設定
 require 'nvim-treesitter.install'.compilers={"gcc"}
 require 'nvim-treesitter.configs'.setup {
-	ensure_installed = "maintained",
+	ensure_installed = { "c","html","javascript","tsx","css","c_sharp","vim","lua","rust" },
 	highlight = {
 		enable = true,
-		},
+		}
 }
