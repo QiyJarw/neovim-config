e820430d3a81b2fcbc954dc5b09e97a5d2ff553e
QiyJarw <mitsurun0515@yahoo.co.jp>
2022-04-29 09:43:56 +0900

    update: テーマを変更。

--- lua/plugins/background.lua
+++ lua/plugins/background.lua
@@ -12,6 +12,8 @@ vim.cmd([[set termguicolors]])
 -- 	 ]])
 -- end
 
-vim.cmd([[colorscheme onedark]])
+-- vim.g.vscode_style = "dark"
+-- vim.g.vscode_transparent= 1;
+vim.cmd([[colorscheme iceberg]])
 vim.bo.syntax='on' --透過設定の後ろでないと動かない
 -- set termguicolors"背景を染めたい時に有効化
