673f409919f975c22b14d24ca1327104c4314056
QiyJarw <m1t2t3ch@gmail.com>
2022-10-04 11:12:47 +0000

    update: added border to documentation.

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -16,6 +16,9 @@ cmp.setup({
 			end
 		})
 	},
+	window = {
+		documentation = cmp.config.window.bordered()
+	},
 	mapping = {
 		['<C-p>'] = cmp.mapping.select_prev_item(),
 		['<C-n>'] = cmp.mapping.select_next_item(),
