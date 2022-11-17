70f788eede8fb105a428f6ce2138d0a88a5abefb
QiyJarw <m1t2t3ch@gmail.com>
2022-11-17 01:07:49 +0000

    remove: removed unnecessary comments.

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,19 +1,5 @@
---背景透過設定:neovide時は無効
 vim.o.termguicolors = true
--- if vim.fn.has('win32') ==1 and vim.g.neovide ~=true then
--- 	 vim.cmd([[augroup TransparentBG
--- 		 autocmd!
--- 		 autocmd Colorscheme * highlight Normal ctermbg=none
--- 		 autocmd Colorscheme * highlight NonText ctermbg=none
--- 		 autocmd Colorscheme * highlight LineNr ctermbg=none
--- 		 autocmd Colorscheme * highlight Folded ctermbg=none
--- 		 autocmd Colorscheme * highlight EndOfBuffer ctermbg=none
--- 	 augroup END
--- 	 ]])
--- end
-
-vim.g.vscode_style = "dark"
+-- vim.g.vscode_style = "dark"
 -- vim.g.vscode_transparent= 1;
-vim.cmd([[colorscheme vscode]])
+vim.cmd([[colorscheme nightfox]])
 vim.bo.syntax='on' --透過設定の後ろでないと動かない
--- set termguicolors"背景を染めたい時に有効化
