e4897e7ed2a3e5801faceafcdfa7df59b72fd753
QiyJarw <mitsurun0515@yahoo.co.jp>
2022-12-03 11:01:37 +0900

    update: activate the plugin config.

--- plugin/alpha.lua
+++ plugin/alpha.lua
@@ -1,27 +1,37 @@
--- local alpha = require('alpha')
--- local dashboard = require('alpha.themes.dashboard')
+local alphaStatus, alpha = pcall(require, "alpha")
+if (not alphaStatus ) then return end
 
--- dashboard.section.buttons.val = {
---     dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
---     dashboard.button( "SPC f f", "  > Find file", ":cd $HOME/dev| Telescope find_files<CR>"),
---     dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
--- 	dashboard.button("SPC o s","  > Open Session", "<cmd>SearchSession<CR>"),
---     dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
---     dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
--- }
+local dashboard = require("alpha.themes.dashboard")
 
--- dashboard.section.header.val = {
--- [[   _______                    .__           ]],
--- [[   \      \   ____  _______  _|__| _____    ]],
--- [[   /   |   \_/ __ \/  _ \  \/ /  |/     \   ]],
--- [[  /    |    \  ___(  <_> )   /|  |  Y Y  \  ]],
--- [[  \____|__  /\___  >____/ \_/ |__|__|_|  /  ]],
--- [[          \/     \/                    \/   ]],
+dashboard.section.buttons.val = {
+    dashboard.button( "e", "  > New file" , ":ene <BAR> startinsert <CR>"),
+    dashboard.button( "SPC f f", "  > Find file", ":cd $HOME/dev| Telescope find_files<CR>"),
+    dashboard.button( "r", "  > Recent"   , ":Telescope oldfiles<CR>"),
+	dashboard.button("SPC o s","  > Open Session", "<cmd>SearchSession<CR>"),
+    dashboard.button( "s", "  > Settings" , ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+    dashboard.button( "q", "  > Quit NVIM", ":qa<CR>"),
+}
 
--- }
--- alpha.setup(dashboard.opts)
+dashboard.section.header.val = {
+[[   _______                    .__           ]],
+[[   \      \   ____  _______  _|__| _____    ]],
+[[   /   |   \_/ __ \/  _ \  \/ /  |/     \   ]],
+[[  /    |    \  ___(  <_> )   /|  |  Y Y  \  ]],
+[[  \____|__  /\___  >____/ \_/ |__|__|_|  /  ]],
+[[          \/     \/                    \/   ]],
 
--- -- Disable folding on alpha buffer
+}
+alpha.setup(dashboard.config)
+
+-- Disable folding on alpha buffer
+
+vim.api.nvim_create_autocmd('FileType', {
+	pattern = "alpha",
+	callback = function ()
+		-- vim.opt_local.foldenable = false
+	end
+})
+
 -- vim.cmd([[
 --     autocmd FileType alpha setlocal nofoldenable
 -- 	autocmd FileType alpha set showtabline=0 | autocmd WinLeave <buffer> set showtabline=2
