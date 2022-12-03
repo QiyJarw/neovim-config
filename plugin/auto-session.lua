fffef95eef48a79b7b8158440977b5c1d5c4a579
QiyJarw <mitsurun0515@yahoo.co.jp>
2022-12-03 10:58:27 +0900

    update: changed a setting.

--- plugin/auto-session.lua
+++ plugin/auto-session.lua
@@ -12,8 +12,8 @@ autoSession.setup{
 	auto_session_enable_last_session = false,
 	auto_session_root_dir = vim.fn.stdpath('data').."/sessions/",
 	auto_session_enabled = true,
-	auto_save_enabled = true,
-	auto_restore_enabled = nil,
+	-- auto_save_enabled = true,
+	-- auto_restore_enabled = nil,
 
 }
 
