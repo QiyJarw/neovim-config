314e558cf329b278f1d34dc8e4a98ab108cabe94
QiyJarw <m1t2t3ch@gmail.com>
2023-04-02 02:22:12 +0000

    add: new plugins.

--- plugin/nvim-dap.lua
+++ plugin/nvim-dap.lua
@@ -1,28 +1,97 @@
-local status, dap = pcall(require, "dap")
-if ( not status ) then return end
+local dapStatus, dap = pcall(require, "dap")
+if (not dapStatus) then return end
+local masonStatus, mason = pcall(require, "mason")
+if (not masonStatus) then return end
+local masonDapStatus, masonDap = pcall(require, "mason-nvim-dap")
+if (not masonDapStatus) then return end
+local uiStatus, dapui = pcall(require, "dapui")
+if (not uiStatus) then return end
 
-dap.adapters.lldb = {
-  type = 'executable',
-  command = '/home/linuxbrew/.linuxbrew/bin/lldb-vscode', -- adjust as needed, must be absolute path
-  name = 'lldb'
+mason.setup {}
+masonDap.setup {
+	ensure_installed = { 'cppdbg' },
+	automatic_installation = true,
+	automatic_setup = true
 }
 
-dap.configurations.cpp = {
-  {
-    name = 'Launch',
-    type = 'lldb',
-    request = 'launch',
-    program = function()
-      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-    end,
-    cwd = '${workspaceFolder}',
-    stopOnEntry = false,
-    args = {},
+masonDap.setup_handlers {
+	function(source_name)
+		require('mason-nvim-dap.automatic_setup')(source_name)
+	end,
+	cppdbg = function(source_name)
+		dap.adapters.cppdbg = {
+			id = "cppdbg", -- necessary for cpptools
+			type = "executable",
+			command = "OpenDebugAD7"
+		}
 
-  },
+		dap.configurations.cpp = {
+			{
+				name = 'Launch',
+				type = 'cppdbg',
+				request = 'launch',
+				linux = {
+					MIMode = 'gdb',
+					miDebuggerPath = 'gdb'
+				},
+				osx = {
+					MIMode = 'lldb',
+					miDebuggerPath = 'lldb'
+				},
+				windows = {
+					MIMode = 'gdb',
+					miDebuggerPath = 'gdb'
+				},
+				program = function()
+					return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+				end,
+				cwd = '${workspaceFolder}',
+				stopOnEntry = false,
+				args = function()
+					local arg = {}
+					vim.ui.input({ prompt = 'enter args:', default = '' }, function(input)
+						if input ~= '' then table.insert(arg,input) end
+					end)
+					return arg
+				end
+			},
+			{
+				name = 'Attach to gdbserver :1234',
+				type = 'cppdbg',
+				request = 'launch',
+				MIMode = 'gdb',
+				miDebuggerServerAddress = 'localhost:1234',
+				miDebuggerPath = 'gdb',
+				cwd = '${workspaceFolder}',
+				program = function()
+					return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+				end,
+			},
+		}
+
+		-- If you want to use this for Rust and C, add something like this:
+		dap.configurations.c = dap.configurations.cpp
+		dap.configurations.rust = dap.configurations.cpp
+	end
 }
 
--- If you want to use this for Rust and C, add something like this:
+dapui.setup {}
+dap.listeners.before['event_initialized']['custom'] = function(session, body)
+	dapui.open()
+end
 
-dap.configurations.c = dap.configurations.cpp
-dap.configurations.rust = dap.configurations.cpp
+dap.listeners.before['event_terminated']['custom'] = function(session, body)
+	dapui.close()
+end
+
+vim.api.nvim_set_hl(0, 'DapStoppedLine', { default = false, fg = 'black', bg = '#d5e63e' })
+vim.api.nvim_set_hl(0, 'DapBreakOutPoint', { default = false, fg = 'red', bg = '' })
+vim.fn.sign_define('DapBreakOutPoint', { text = '', texthl = 'DapBreakOutPoint', linehl = '', numhl = '' })
+vim.fn.sign_define('DapStopped', { text = '', texthl = '', linehl = 'DapStoppedLine', numhl = '' })
+
+local opts = { silent = true }
+vim.keymap.set('n', '<F5>', '<cmd>DapContinue<CR>', opts)
+vim.keymap.set('n', '<F6>', '<cmd>DapStepOver<CR>', opts)
+vim.keymap.set('n', '<F7>', '<cmd>DapStepInto<CR>', opts)
+vim.keymap.set('n', '<F8>', '<cmd>DapStepOut<CR>', opts)
+vim.keymap.set('n', '<leader>b', ':DapToggleBreakpoint<CR>', opts)
---
+++ plugin/which-key.lua
@@ -0,0 +1,5 @@
+local status, whichkey = pcall(require, "which-key")
+if (not status) then return end
+vim.o.timeout = true
+vim.o.timeoutlen = 300
+whichkey.setup{}
