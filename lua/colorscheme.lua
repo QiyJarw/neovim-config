010d111eeade2ad2cb0f99551d8b95c20ee3e724
QiyJarw <m1t2t3ch@gmail.com>
2023-04-02 02:24:51 +0000

    update: changed colorscheme.

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,5 +1,5 @@
 vim.o.termguicolors = true
--- vim.g.vscode_style = "dark"
+vim.g.vscode_style = "dark"
 -- vim.g.vscode_transparent= 1;
 vim.cmd([[colorscheme nightfox]])
 vim.bo.syntax='on' --透過設定の後ろでないと動かない
