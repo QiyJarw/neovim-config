fdcab5a39d0c3fd84596fd265b977df8e2e1baf6
QiyJarw <m1t2t3ch@gmail.com>
2023-09-27 05:44:12 +0000

    update: parser auto installation enabled.

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -10,6 +10,8 @@ if ( not autoTagStatus ) then return end
 
 configs.setup {
 	ensure_installed = { "vim","lua","markdown", "markdown_inline"},
+	sync_install = true,
+	auto_install = true,
 	highlight = {
 		enable = true,
 		}
